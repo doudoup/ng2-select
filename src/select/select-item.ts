a2215e66c56cacfcc94ca349c0515718fd6eeb3e
jasminexie <ameliasgoldfish@gmail.com>
2018-10-15 15:32:41 +0800

    feat: upgrade to angular 6

--- src/select/select-item.ts
+++ src/select/select-item.ts
@@ -9,7 +9,8 @@ export class SelectItem {
       this.id = this.text = source;
     }
     if (typeof source === 'object') {
-      this.id = (typeof source.id === 'undefined' || source.id === null ? source.text : source.id);
+      this.id = (typeof source.id === 'undefined' || source.id === null) ? source.text : source.id;
+      console.log(this.id);
       this.text = source.text;
       if (source.children && source.text) {
         this.children = source.children.map((c:any) => {
